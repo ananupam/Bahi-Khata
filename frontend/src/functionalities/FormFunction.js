cfc410e891f538a78bb74996e5ad3967969be1b0
Anjali Anupam <2019024@iiitdmj.ac.in>
2022-10-28 15:34:58 +0530

    adding form functionalities

---
+++ frontend/src/functionalities/FormFunction.js
@@ -0,0 +1,25 @@
+export const add_record = "add_record";
+export const addRecord = (record) => {
+    return {
+        type: add_record,
+        record,
+    };
+};
+
+export const delete_record = "delete_record";
+export const deleteRecord = (id) => {
+    return {
+        type: delete_record,
+        id,
+    };
+};
+
+
+export const edit_record = "edit_record";
+export const editRecord = (id, newRecord) => {
+    return {
+        type: edit_record,
+        id,
+        newRecord,
+    };
+};
